335099bfe7f0b27fa59f037868ba5406160c2776
TobiasAagaard <Tobias909@live.dk>
2025-03-05 14:22:31 +0100

    Animation to navbar

--- Frontend/src/Components/Navbar/Navbar.tsx
+++ Frontend/src/Components/Navbar/Navbar.tsx
@@ -2,6 +2,8 @@
 import { NavLink } from "react-router-dom"
 import { BurgerMenu } from "../BurgerMenu/BurgerMenu"
 
+import langImg from "../../assets/language-svgrepo-com (1).svg"
+
 import "./Navbar.scss"
 
 export const Navbar: React.FC = () => {
@@ -11,8 +13,10 @@ export const Navbar: React.FC = () => {
                 <li><NavLink to={"/"}>Forside</NavLink></li>
                 <li><NavLink to={"/projects"}>Projekter</NavLink></li>
             </ul>
-
-
+            
+        <div>
+            <img src={langImg} alt="" />
+        </div>
         <BurgerMenu />
         </nav>
     )
--- Frontend/src/Pages/Frontpage/Frontpage.tsx
+++ Frontend/src/Pages/Frontpage/Frontpage.tsx
@@ -1,8 +1,10 @@
+import myImg from "../../assets/mig.jpg"
+
 
 export const Frontend: React.FC = () => {
     return (
         <header>
-            
+            <img src={myImg} width={"200px"} alt="" />
         </header>
     )
 }
