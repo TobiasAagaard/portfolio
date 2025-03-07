e73b03062d64252cf80850cf3cf1d3e3cc326dcf
TobiasAagaard <Tobias909@live.dk>
2025-03-08 00:33:24 +0100

    Navbar done for now

--- Frontend/src/Components/Navbar/Navbar.tsx
+++ Frontend/src/Components/Navbar/Navbar.tsx
@@ -9,10 +9,12 @@ export const Navbar: React.FC = () => {
 
     return (
         <nav className="mainNav">
-            <ul className="mainUl">
-                <li><NavLink to={"/"}>Forside</NavLink></li>
-                <li><NavLink to={"/projects"}>Projekter</NavLink></li>
-            </ul>
+       <ul className="mainUl">
+            <li><NavLink to="/" className={({ isActive }) => isActive ? "active" : ""}>Home</NavLink></li>
+            <li><NavLink to="/projects" className={({ isActive }) => isActive ? "active" : ""}>Projects</NavLink></li>
+            <li><NavLink to="/about" className={({ isActive }) => isActive ? "active" : ""}>Om mog</NavLink></li>
+            <li><NavLink to="/contact" className={({ isActive }) => isActive ? "active" : ""}>Kontakt</NavLink></li>
+      </ul>
             
         <BurgerMenu />
         </nav>
