108b5bb62249680b108539f2395ab0bdf38b3161
TobiasAagaard <Tobias909@live.dk>
2025-03-10 22:36:35 +0100

    header section done

---
+++ Frontend/src/Components/Header/Header.tsx
@@ -0,0 +1,47 @@
+import { useTranslation } from "react-i18next"
+import { useTypewriter} from "react-simple-typewriter"
+import { motion } from "framer-motion";
+import myImg from "../../assets/mig.jpg"
+import "./Header.scss"
+
+
+export const Header: React.FC = () => {
+    const {t} = useTranslation();
+    const words:string[] = [
+
+        t("typewriter.first"),
+        t("typewriter.second"),
+        t("typewriter.third")
+    ];
+
+    const [text] = useTypewriter({
+        words,
+        loop: true,
+        typeSpeed: 120,
+        delaySpeed: 2500,
+       
+    });
+    return (
+        <motion.header
+        initial={{ opacity: 0, y: -20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 1, ease: "easeOut" }} >
+            <article>
+                <motion.h2
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1.2, ease: "easeOut", delay: 0.2 }}>{t('welcomeMessage')}</motion.h2>
+                <motion.h2
+                    initial={{ opacity: 0, x: -20 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1.2, ease: "easeOut", delay: 0.4 }}>{t('introText')}{" "}<span className="writerBox">{text?.toString() || "\u00A0"}</span></motion.h2>
+            </article>
+            <motion.img src={myImg}
+             width={"200px"}
+             alt="Image of me" 
+             initial={{ opacity: 0}}
+             animate={{ opacity: 1}}
+             transition={{ duration: 1.2, ease: "easeOut", delay: 0.6 }}/>
+        </motion.header>
+    )
+}
--- Frontend/src/Pages/Frontpage/Frontpage.tsx
+++ Frontend/src/Pages/Frontpage/Frontpage.tsx
@@ -1,32 +1,8 @@
-import { useTranslation } from "react-i18next"
-import { useTypewriter} from "react-simple-typewriter"
-import myImg from "../../assets/mig.jpg"
-import "./Frontpage.scss"
+import { Header } from "../../Components/Header/Header"
 
 
 export const Frontend: React.FC = () => {
-    const {t} = useTranslation();
-    const words:string[] = [
-
-        t("typewriter.first"),
-        t("typewriter.second"),
-        t("typewriter.third")
-    ];
-
-    const [text] = useTypewriter({
-        words,
-        loop: true,
-        typeSpeed: 100,
-        delaySpeed: 5500,
-       
-    });
     return (
-        <header>
-            <article>
-                <h2>{t('welcomeMessage')}</h2>
-                <h2>{t('introText')}{" "}<span className="writerBox">{text?.toString() || "\u00A0"}</span></h2>
-            </article>
-            <img src={myImg} width={"200px"} alt="" />
-        </header>
+        <Header />
     )
 }
