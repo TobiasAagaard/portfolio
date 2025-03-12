9b32d5ae9cadbc39a8012d2aac55fdba94a9b074
TobiasAagaard <Tobias909@live.dk>
2025-03-12 12:33:30 +0100

    Language save in local storage

--- Frontend/src/Components/LanguageSelector/LanguageSelector.tsx
+++ Frontend/src/Components/LanguageSelector/LanguageSelector.tsx
@@ -1,3 +1,6 @@
+
+import { ul } from "framer-motion/client"
+import { useEffect } from "react"
 import { useTranslation } from "react-i18next"
 
 interface Language {
@@ -15,13 +18,23 @@ const language: Language[] = [
 
 export const LanguageSelector: React.FC = () => {
     const {i18n} = useTranslation()
+
+    useEffect(() => {
+        const savedLanguage = localStorage.getItem("language");
+        if (savedLanguage) {
+            i18n.changeLanguage(savedLanguage);
+        }
+    }, [i18n]);
     
 const changeLanguage = (lng: string) => {
     i18n.changeLanguage(lng)
+    localStorage.setItem("language", lng)
 }
     return (
+        
         language.map((lng) => {
-            return <button key={lng.code} onClick={() => changeLanguage(lng.code)}>{lng.lang}</button>
-        })
+            return (
+            <button key={lng.code} onClick={() => changeLanguage(lng.code)}>{lng.lang}</button>
+        )})
     )
 }
