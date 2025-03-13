19929a2402fcd82c47226cd0d24e3d067fcbe04c
TobiasAagaard <Tobias909@live.dk>
2025-03-13 14:36:16 +0100

    Pagewrapper done

--- Frontend/src/Layout/PageWrapper/PageWrapper.tsx
+++ Frontend/src/Layout/PageWrapper/PageWrapper.tsx
@@ -1,7 +1,28 @@
-import { ReactNode } from "react";
+import { ReactNode, useEffect } from "react";
 
 interface PageWrapperProps {
     title: string;
     description: string;
     children: ReactNode;
 }
+
+export const PageWrapper: React.FC<PageWrapperProps> = ({ title, description, children }) => {
+    useEffect(() => {
+        document.title = title;
+
+        let metaDescription = document.querySelector("meta[name='description']");
+        if (metaDescription) {
+            metaDescription.setAttribute("content", description);
+        } else {
+           
+            metaDescription = document.createElement("meta");
+            metaDescription.setAttribute("name", "description");
+            metaDescription.setAttribute("content", description);
+            document.head.appendChild(metaDescription);
+        }
+    }, [title, description]); 
+
+    return (
+            <div>{children}</div>
+    );
+};
--- Frontend/src/Pages/Frontpage/Frontpage.tsx
+++ Frontend/src/Pages/Frontpage/Frontpage.tsx
@@ -1,12 +1,13 @@
 import { Header } from "../../Components/Header/Header"
 import { Tech } from "../../Components/Tech/Tech"
+import { PageWrapper } from "../../Layout/PageWrapper/PageWrapper"
 
 
 export const Frontend: React.FC = () => {
     return (
-        <>
+            <PageWrapper  title="Portfolio" description="Explore the portfolio of Tobias, a skilled full-stack developer specializing in C#, .NET, React, and network systems. See projects in web, mobile, and software development!">
             <Header />
             <Tech />
-        </>
+            </PageWrapper>
     )
 }
