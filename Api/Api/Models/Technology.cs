c778c3a7059651184de3a76bb0a2c3f11f485f5a
TobiasAagaard <Tobias909@live.dk>
2025-03-29 14:56:38 +0100

    Tech models

---
+++ Api/Api/Models/Technology.cs
@@ -0,0 +1,10 @@
+namespace api.Models
+{
+    public class Technology
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public string Icon { get; set; }
+    public string Color { get; set; }
+}
+}
