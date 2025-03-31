70526ea071657e8e8d8ad815183d6b541af00820
TobiasAagaard <Tobias909@live.dk>
2025-03-31 12:20:23 +0200

    Github icon added to footer as social propety in footer

--- Frontend/src/Components/Footer/Footer.tsx
+++ Frontend/src/Components/Footer/Footer.tsx
@@ -1,9 +1,18 @@
 import { JSX } from "react";
+import { SiGithub } from "react-icons/si";
 interface SocialsIcons {
     name: string;
     icon: JSX.Element;
     link: string;
 }
+
+const socials: SocialsIcons[] = [
+    {
+        name: "GitHub",
+        icon: <SiGithub />,
+        link: "https://github.com/TobiasAagaard" 
+    },
+]
 
 
 
