32a3bc880536c70f9384fe7de3389efbf2d5f2bf
TobiasAagaard <Tobias909@live.dk>
2025-03-31 14:50:39 +0200

    one of last commits of the day

--- Frontend/src/Pages/Frontpage/Frontpage.tsx
+++ Frontend/src/Pages/Frontpage/Frontpage.tsx
@@ -5,7 +5,7 @@ import { PageWrapper } from "../../Layout/PageWrapper/PageWrapper"
 
 export const Frontend: React.FC = () => {
     return (
-            <PageWrapper  title="Portfolio" description="Explore the portfolio of Tobias, a skilled full-stack developer specializing in C#, .NET, React, and network systems. See projects in web, mobile, and software development!">
+            <PageWrapper  title="Frontpage | Tobias Portfolio " description="Explore the portfolio of Tobias, a skilled full-stack developer specializing in C#, .NET, React, and network systems. See projects in web, mobile, and software development!">
             <Header />
             <Tech />
             </PageWrapper>
