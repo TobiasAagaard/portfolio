25ba9ee3a6591aed7859ccbae425d3274794062b
TobiasAagaard <Tobias909@live.dk>
2025-04-06 12:49:19 +0200

    Model update := CreatedAt and UpdatedAttime.Time package

--- Api/models/project.go
+++ Api/models/project.go
@@ -1,10 +1,14 @@
 package models
 
+import "time"
+
 type Project struct {
 	ID          int
 	Title       string
 	Description string
 	TechStack   []string
+	CreatedAt   time.Time
+	UpdatedAt   time.Time
 	Image       string
 	Slug        string
 }
