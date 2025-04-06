01aabbdfb4e878f44aa97f0ba858a35b2ca5e1e5
TobiasAagaard <Tobias909@live.dk>
2025-04-06 13:06:38 +0200

    Save function setup for later database setup and nice project model done

--- Api/models/project.go
+++ Api/models/project.go
@@ -7,8 +7,18 @@ type Project struct {
 	Title       string
 	Description string
 	TechStack   []string
+	GithubLink  string
 	CreatedAt   time.Time
 	UpdatedAt   time.Time
-	Image       string
+	ImageURLs   []string
+	IsFeatured  bool
 	Slug        string
+	UserId      int
+}
+
+var projects = []Project{}
+
+func (e Project) Save() {
+	//Later: add to Database
+	projects = append(projects, e)
 }
