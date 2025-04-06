71f0061d0d181985739d4c50d1a0a9890ceb26d2
TobiasAagaard <Tobias909@live.dk>
2025-04-06 12:31:25 +0200

    Api setup -- Backend work

--- Api/main.go
+++ Api/main.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"net/http"
+
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	router := gin.Default()
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "this is my api",
-		})
-	})
-	router.Run("localhost:8080")
+	server := gin.Default()
+
+	server.GET("/projects", getProjects)
+
+	server.Run(":8080") //Localhost:8080
+}
+
+func getProjects(context *gin.Context) {
+	context.JSON(http.StatusOK, gin.H{"message": "Hello this is a project function"})
 }
