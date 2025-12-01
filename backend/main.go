9e4738777fbbd5fd254cc77de107862a7ec93bdb
TobiasAagaard <tach@trifork.com>
2025-12-01 13:58:48 +0100

    init gin framework

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,19 @@
 package main
 
 import (
-	fmt "fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	fmt.Println("Init of my backend portfolio")
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run() // listen and serve on
 }
